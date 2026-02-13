6dd013f3a820c4f8d4b9a743e266246880f48fb5
Mustyjide19 <mustyjide19@gmail.com>
2026-02-13 17:23:39 +0100

    Secure Firebase config with environment variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,21 +4,18 @@ import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA635033ARrFxumANObsLFi1h3AfMNCwQI",
-  authDomain: "envoi-fb546.firebaseapp.com",
-  projectId: "envoi-fb546",
-  storageBucket: "envoi-fb546.firebasestorage.app",
-  messagingSenderId: "1006106652724",
-  appId: "1:1006106652724:web:a0cdcb986d7c840e455a61",
-  measurementId: "G-0DTNF6GBW9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
-
 // const analytics = getAnalytics(app);
